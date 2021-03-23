e7e94357d59d2a72102170b9e38cb1afe51fa4c2
akmantuna <akmantuna0203@gmail.com>
2021-03-24 00:36:07 +0300

    Add .modal Class CSS And Modal Open & Close Event JavaScript

--- framework.js
+++ framework.js
@@ -7,6 +7,12 @@ window.addEventListener('load', () => {
             case 'alert':
                 document.querySelector(`[data-alert-key="${$el.dataset.alertKey}"]`).remove()
                 break;
+            case 'modal':
+                if ($el.dataset.modalType === 'open')
+                    document.querySelector(`.modal[data-modal-key="${$el.dataset.modalKey}"]`).classList.add('show')
+                else if ($el.dataset.modalType === 'close')
+                    document.querySelector(`.modal[data-modal-key="${$el.dataset.modalKey}"]`).classList.remove('show')
+                break;
         }
     })
 
