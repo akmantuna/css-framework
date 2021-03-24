85cbe41eac51d556b19dfa3565a75fb96ee9d608
akmantuna <akmantuna0203@gmail.com>
2021-03-24 15:25:58 +0300

    Update framework.js File

--- framework.js
+++ framework.js
@@ -6,13 +6,13 @@ window.addEventListener('load', () => {
         switch (el.target.dataset.eventType) {
             case 'alert':
                 document.querySelector(`[data-alert-key="${$el.dataset.alertKey}"]`).remove()
-                break;
+                break
             case 'modal':
                 if ($el.dataset.modalType === 'open')
                     document.querySelector(`.modal[data-modal-key="${$el.dataset.modalKey}"]`).classList.add('show')
                 else if ($el.dataset.modalType === 'close')
                     document.querySelector(`.modal[data-modal-key="${$el.dataset.modalKey}"]`).classList.remove('show')
-                break;
+                break
         }
     })
 
